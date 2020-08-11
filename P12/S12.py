acf417434fb7629a15d63d4712373e01a15e289f
Rishabh Singhal <r.singhal.com@gmail.com>
2020-08-11 18:28:59 +0530

    11/Aug

--- P12/S12.py
+++ P12/S12.py
@@ -1,6 +1,7 @@
 import re
 
 def VowelMatch(s):
+    No_match = True
     for i in re.finditer("[aeiouAEIOU][aeiouAEIOU]+",s):
         start = i.span()[0]
         end = i.span()[1]
@@ -8,7 +9,11 @@ def VowelMatch(s):
             bool_start = re.match("[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]",s[start-1])
             bool_end = re.match("[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]",s[end])
             if((bool_start != None) and (bool_end != None)):
+                No_match = False
                 print(i.group())
+
+    if(No_match):
+        print(-1)
 
 if __name__== '__main__':
     s = str(input())
