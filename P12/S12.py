47746b83e5a5380237be465c63a9962e2feb0c0a
Rishabh Singhal <r.singhal.com@gmail.com>
2020-08-11 18:25:33 +0530

    11/Aug

---
+++ P12/S12.py
@@ -0,0 +1,15 @@
+import re
+
+def VowelMatch(s):
+    for i in re.finditer("[aeiouAEIOU][aeiouAEIOU]+",s):
+        start = i.span()[0]
+        end = i.span()[1]
+        if((start != 0 ) and (end != len(s))):
+            bool_start = re.match("[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]",s[start-1])
+            bool_end = re.match("[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]",s[end])
+            if((bool_start != None) and (bool_end != None)):
+                print(i.group())
+
+if __name__== '__main__':
+    s = str(input())
+    VowelMatch(s)
